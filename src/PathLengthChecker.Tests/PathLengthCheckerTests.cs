79625da5a0c49ccfb03c6417fc7f652d2f6c0dd5
deadlydog <deadlydog@hotmail.com>
2017-06-26 01:35:35 -0600

    Move source code into src directory, and delete temporary files.

---
+++ src/PathLengthChecker.Tests/PathLengthCheckerTests.cs
@@ -0,0 +1,429 @@
+﻿using System.Linq;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace PathLengthChecker.Tests
+{
+	/// <summary>
+	/// Tests to test the PathLengthChecker class.
+	/// </summary>
+	[TestClass()]
+	public class PathLengthCheckerTests
+	{
+		/// <summary>
+		///Gets or sets the test context which provides
+		///information about and functionality for the current test run.
+		///</summary>
+		public TestContext TestContext { get; set; }
+
+		private static List<PathInfo> _directories;
+		private static List<PathInfo> _files;
+		private static List<PathInfo> _allPaths;
+		private static readonly string RootPath = Path.Combine(Environment.CurrentDirectory, "UnitTestTemp");
+		private static readonly string EmptyDirectoryPath = Path.Combine(RootPath, "EmptyDir");
+
+		#region Additional test attributes
+		/// <summary>
+		/// You can use the following additional attributes as you write your tests:
+		/// Use ClassInitialize to run code before running the first test in the class
+		/// </summary>
+		/// <param name="testContext">The test context.</param>
+		[ClassInitialize()]
+		public static void MyClassInitialize(TestContext testContext)
+		{
+			// Specify the directories and files that the unit tests expect to be there.
+			_directories = new List<PathInfo>()
+			               	{
+			               		new PathInfo(){Path = Path.Combine(RootPath, "TestDir1")},
+								new PathInfo(){Path = Path.Combine(RootPath, "TestDir2")}, 
+								new PathInfo(){Path = Path.Combine(RootPath, "TestDir2\\TestDir3")},
+								new PathInfo(){Path = EmptyDirectoryPath}
+			               	};
+
+			_files = new List<PathInfo>()
+			         	{
+							new PathInfo(){Path = Path.Combine(RootPath, "TestFile0.test")},
+							new PathInfo(){Path = Path.Combine(RootPath, "TestDir1\\TestFile1.test")},
+							new PathInfo(){Path = Path.Combine(RootPath, "TestDir2\\TestFile2.test")},
+							new PathInfo(){Path = Path.Combine(RootPath, "TestDir2\\TestDir3\\TestFile3.test")}
+			         	};
+
+			// Create a list that contains all paths.
+			_allPaths = new List<PathInfo>(_directories);
+			_allPaths.AddRange(_files);
+
+			// Create the paths on the local hard drive.
+			CreateDirectoriesAndFiles();
+		}
+
+		/// <summary>
+		/// Use ClassCleanup to run code after all tests in a class have run
+		/// </summary>
+		[ClassCleanup()]
+		public static void MyClassCleanup()
+		{
+			DeleteDirectoriesAndFiles();
+		}
+
+		//Use TestInitialize to run code before running each test
+		//[TestInitialize()]
+		//public void MyTestInitialize()
+		//{
+		//}
+		//
+		//Use TestCleanup to run code after each test has run
+		//[TestCleanup()]
+		//public void MyTestCleanup()
+		//{
+		//}
+		//
+
+		private static void CreateDirectoriesAndFiles()
+		{
+			// Create the directories and files that the unit tests expect to be there.
+			foreach (var diretory in _directories)
+			{
+				var directoryPath = Path.Combine(RootPath, diretory.Path);
+				if (!Directory.Exists(directoryPath))
+					Directory.CreateDirectory(directoryPath);
+			}
+
+			foreach (var file in _files)
+			{
+				var filePath = Path.Combine(RootPath, file.Path);
+				if (!File.Exists(filePath))
+					File.Create(filePath);
+			}
+		}
+
+		private static void DeleteDirectoriesAndFiles()
+		{
+			// Delete the directories and files created for the unit tests.
+			foreach (var file in _files)
+			{
+				var filePath = Path.Combine(RootPath, file.Path);
+				if (File.Exists(filePath))
+					File.Delete(filePath);
+			}
+
+			foreach (var diretory in _directories)
+			{
+				var directoryPath = Path.Combine(RootPath, diretory.Path);
+				if (Directory.Exists(directoryPath))
+					Directory.Delete(directoryPath, true);
+			}
+		}
+		#endregion
+
+
+		/// <summary>
+		///A test for PathCollector Constructor
+		///</summary>
+		[TestMethod()]
+		public void GetAllPaths()
+		{
+			// Setup the Search Options
+			var searchOptions = new PathLengthSearchOptions()
+									{
+										RootDirectory = RootPath,
+										SearchOption = SearchOption.AllDirectories,
+										TypesToGet = FileSystemTypes.All,
+										SearchPattern = string.Empty,
+										RootDirectoryReplacement = null,
+										MinimumPathLength = -1,
+										MaximumPathLength = -1
+									};
+
+			// Because we try and get a list of paths.
+			var paths = PathLengthChecker.GetPathsWithLengths(searchOptions);
+
+			// We should have all of the paths.
+			paths.Should().Contain(_directories).And.Contain(_files);
+
+			// And we should not have any extra paths.
+			paths.Should().OnlyContain(p => _directories.Contains(p) || _files.Contains(p));
+		}
+
+		[TestMethod]
+		public void GetAllDirectories()
+		{
+			// Setup the Search Options
+			var searchOptions = new PathLengthSearchOptions()
+			{
+				RootDirectory = RootPath,
+				SearchOption = SearchOption.AllDirectories,
+				TypesToGet = FileSystemTypes.Directories,
+				SearchPattern = string.Empty,
+				RootDirectoryReplacement = null,
+				MinimumPathLength = -1,
+				MaximumPathLength = -1
+			};
+
+			// Because we try and get a list of directory paths.
+			var paths = PathLengthChecker.GetPathsWithLengths(searchOptions);
+
+			// We should have all of the directory paths.
+			paths.Should().Contain(_directories);
+
+			// And we should not have any extra paths.
+			paths.Should().OnlyContain(p => _directories.Contains(p));
+		}
+
+		[TestMethod]
+		public void GetAllFiles()
+		{
+			// Setup the Search Options
+			var searchOptions = new PathLengthSearchOptions()
+			{
+				RootDirectory = RootPath,
+				SearchOption = SearchOption.AllDirectories,
+				TypesToGet = FileSystemTypes.Files,
+				SearchPattern = string.Empty,
+				RootDirectoryReplacement = null,
+				MinimumPathLength = -1,
+				MaximumPathLength = -1
+			};
+
+			// Because we try and get a list of file paths.
+			var paths = PathLengthChecker.GetPathsWithLengths(searchOptions);
+
+			// We should have all of the file paths.
+			paths.Should().Contain(_files);
+
+			// And we should not have any extra paths.
+			paths.Should().OnlyContain(p => _files.Contains(p));
+		}
+
+		[TestMethod]
+		public void GetAllPathsInTopLevelDirectory()
+		{
+			// Setup the Search Options
+			var searchOptions = new PathLengthSearchOptions()
+			{
+				RootDirectory = RootPath,
+				SearchOption = SearchOption.TopDirectoryOnly,
+				TypesToGet = FileSystemTypes.All,
+				SearchPattern = string.Empty,
+				RootDirectoryReplacement = null,
+				MinimumPathLength = -1,
+				MaximumPathLength = -1
+			};
+
+			// Because we try and get a list of paths.
+			var paths = PathLengthChecker.GetPathsWithLengths(searchOptions);
+
+			// We should have all of the paths directly off of the Root Path.
+			paths.Should().Contain(p => string.Equals(Path.GetDirectoryName(p.Path), RootPath));
+
+			// And we should not have any extra paths.
+			paths.Should().OnlyContain(p => string.Equals(Path.GetDirectoryName(p.Path), RootPath));
+		}
+
+		[TestMethod]
+		public void GetAllDirectoriesInTopLevelDirectory()
+		{
+			// Setup the Search Options
+			var searchOptions = new PathLengthSearchOptions()
+			{
+				RootDirectory = RootPath,
+				SearchOption = SearchOption.TopDirectoryOnly,
+				TypesToGet = FileSystemTypes.Directories,
+				SearchPattern = string.Empty,
+				RootDirectoryReplacement = null,
+				MinimumPathLength = -1,
+				MaximumPathLength = -1
+			};
+
+			// Because we try and get a list of paths.
+			var paths = PathLengthChecker.GetPathsWithLengths(searchOptions);
+
+			// We should have all of the directory paths directly off of the Root Path.
+			paths.Should().Contain(p => string.Equals(Path.GetDirectoryName(p.Path), RootPath));
+
+			// And we should not have any extra paths.
+			paths.Should().OnlyContain(p => string.Equals(Path.GetDirectoryName(p.Path), RootPath));
+		}
+
+		[TestMethod]
+		public void GetAllFilesInTopLevelDirectory()
+		{
+			// Setup the Search Options
+			var searchOptions = new PathLengthSearchOptions()
+			{
+				RootDirectory = RootPath,
+				SearchOption = SearchOption.TopDirectoryOnly,
+				TypesToGet = FileSystemTypes.Files,
+				SearchPattern = string.Empty,
+				RootDirectoryReplacement = null,
+				MinimumPathLength = -1,
+				MaximumPathLength = -1
+			};
+
+			// Because we try and get a list of paths.
+			var paths = PathLengthChecker.GetPathsWithLengths(searchOptions);
+
+			// We should have all of the file paths directly off of the Root Path.
+			paths.Should().Contain(p => string.Equals(Path.GetDirectoryName(p.Path), RootPath));
+
+			// And we should not have any extra paths.
+			paths.Should().OnlyContain(p => string.Equals(Path.GetDirectoryName(p.Path), RootPath));
+		}
+
+		[TestMethod]
+		public void GetAllFilesFromEmptyDirectory()
+		{
+			// Setup the Search Options
+			var searchOptions = new PathLengthSearchOptions()
+			{
+				RootDirectory = EmptyDirectoryPath,
+				SearchOption = SearchOption.AllDirectories,
+				TypesToGet = FileSystemTypes.All,
+				SearchPattern = string.Empty,
+				RootDirectoryReplacement = null,
+				MinimumPathLength = -1,
+				MaximumPathLength = -1
+			};
+
+			// Because we try and get a list of paths from an empty directory.
+			var paths = PathLengthChecker.GetPathsWithLengths(searchOptions);
+
+			// There shouldn't be any paths found.
+			paths.Should().HaveCount(0);
+		}
+
+		[TestMethod]
+		public void InvalidDirectorySoShouldThrowException()
+		{
+			// Setup the Search Options
+			var searchOptions = new PathLengthSearchOptions()
+			{
+				RootDirectory = Path.Combine(RootPath, "ADirectoryThatDoesNotExist"),
+				SearchOption = SearchOption.AllDirectories,
+				TypesToGet = FileSystemTypes.All,
+				SearchPattern = string.Empty,
+				RootDirectoryReplacement = null,
+				MinimumPathLength = -1,
+				MaximumPathLength = -1
+			};
+
+			// Because we try and get a list of paths from a directory that does not exist.
+			Action act = () => PathLengthChecker.GetPathsWithLengths(searchOptions).Count();
+
+			// A DirectoryNotFound exception should be thrown.
+			act.ShouldThrow<DirectoryNotFoundException>();
+		}
+
+		[TestMethod]
+		public void GetAllPathsLessThanXCharacters()
+		{
+			// Get a length that doesn't include all paths, and then get the list of paths that should match the length condition.
+			int maxPathLength = _allPaths.Min(p => p.Length) + 1;
+			var expectedPaths = _allPaths.Where(p => p.Length <= maxPathLength);
+
+			// Setup the Search Options
+			var searchOptions = new PathLengthSearchOptions()
+									{
+										RootDirectory = RootPath,
+										SearchOption = SearchOption.AllDirectories,
+										TypesToGet = FileSystemTypes.All,
+										SearchPattern = string.Empty,
+										RootDirectoryReplacement = null,
+										MinimumPathLength = -1,
+										MaximumPathLength = maxPathLength
+									};
+
+			// Because we try and get a list of paths.
+			var paths = PathLengthChecker.GetPathsWithLengths(searchOptions);
+
+			// We should have all of the paths.
+			paths.Should().Contain(expectedPaths);
+
+			// And we should not have any extra paths.
+			paths.Should().OnlyContain(p => expectedPaths.Contains(p));
+		}
+
+		[TestMethod]
+		public void GetAllPathsMoreThanXCharacters()
+		{
+			// Get a length that doesn't include all paths, and then get the list of paths that should match the length condition.
+			int minPathLength = _allPaths.Max(p => p.Length) - 1;
+			var expectedPaths = _allPaths.Where(p => p.Length >= minPathLength);
+
+			// Setup the Search Options
+			var searchOptions = new PathLengthSearchOptions()
+			{
+				RootDirectory = RootPath,
+				SearchOption = SearchOption.AllDirectories,
+				TypesToGet = FileSystemTypes.All,
+				SearchPattern = string.Empty,
+				RootDirectoryReplacement = null,
+				MinimumPathLength = minPathLength,
+				MaximumPathLength = -1
+			};
+
+			// Because we try and get a list of paths.
+			var paths = PathLengthChecker.GetPathsWithLengths(searchOptions);
+
+			// We should have all of the paths.
+			paths.Should().Contain(expectedPaths);
+
+			// And we should not have any extra paths.
+			paths.Should().OnlyContain(p => expectedPaths.Contains(p));
+		}
+
+		[TestMethod]
+		public void GetAllPathsMoreThanXAndLessThanYCharacters()
+		{
+			// Get a length that doesn't include all paths, and then get the list of paths that should match the length condition.
+			int minPathLength = _allPaths.Min(p => p.Length) + 1;
+			int maxPathLength = _allPaths.Max(p => p.Length) - 1;
+			var expectedPaths = _allPaths.Where(p => p.Length >= minPathLength && p.Length <= maxPathLength);
+
+			// Setup the Search Options
+			var searchOptions = new PathLengthSearchOptions()
+			{
+				RootDirectory = RootPath,
+				SearchOption = SearchOption.AllDirectories,
+				TypesToGet = FileSystemTypes.All,
+				SearchPattern = string.Empty,
+				RootDirectoryReplacement = null,
+				MinimumPathLength = minPathLength,
+				MaximumPathLength = maxPathLength
+			};
+
+			// Because we try and get a list of paths.
+			var paths = PathLengthChecker.GetPathsWithLengths(searchOptions);
+
+			// We should have all of the paths.
+			paths.Should().Contain(expectedPaths);
+
+			// And we should not have any extra paths.
+			paths.Should().OnlyContain(p => expectedPaths.Contains(p));
+		}
+
+		[TestMethod]
+		public void MinimumPathLengthGreaterThanMaximumPathLengthSoShouldThrowException()
+		{
+			// Setup the Search Options
+			var searchOptions = new PathLengthSearchOptions()
+			{
+				RootDirectory = RootPath,
+				SearchOption = SearchOption.AllDirectories,
+				TypesToGet = FileSystemTypes.All,
+				SearchPattern = string.Empty,
+				RootDirectoryReplacement = null,
+				MinimumPathLength = 2,
+				MaximumPathLength = 1
+			};
+
+			// Because we try and get a list of paths from a directory that does not exist.
+			Action act = () => PathLengthChecker.GetPathsWithLengths(searchOptions).Count();
+
+			// A DirectoryNotFound exception should be thrown.
+			act.ShouldThrow<MinPathLengthGreaterThanMaxPathLengthException>();
+		}
+	}
+}
---
+++ src/PathLengthCheckerGUI/MainWindow.xaml.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using PathLengthChecker;
+
+namespace PathLengthCheckerGUI
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+			this.DataContext = this;
+
+			// Set the default type for the combo boxes.
+			cmbTypesToInclude.SelectedValue = FileSystemTypes.All;
+		}
+
+		public List<PathInfo> Paths
+		{
+			get { return (List<PathInfo>)GetValue(PathsProperty); }
+			set { SetValue(PathsProperty, value); }
+		}
+		public static readonly DependencyProperty PathsProperty = DependencyProperty.Register("Paths", typeof(List<PathInfo>), typeof(MainWindow), new UIPropertyMetadata(new List<PathInfo>()));
+
+		/// <summary>
+		/// Handles the Click event of the btnBrowseForRootDirectory control.
+		/// </summary>
+		/// <param name="sender">The source of the event.</param>
+		/// <param name="e">The <see cref="System.Windows.RoutedEventArgs"/> instance containing the event data.</param>
+		private void btnBrowseForRootDirectory_Click(object sender, RoutedEventArgs e)
+		{
+			// Setup the prompt
+			var folderDialog = new System.Windows.Forms.FolderBrowserDialog();
+			folderDialog.Description = "Select the directory that contains the paths whose length you want to check...";
+			folderDialog.ShowNewFolderButton = false;
+
+			// If the user selected a folder, put it in the Root Directory text box.
+			if (folderDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+				txtRootDirectory.Text = folderDialog.SelectedPath;
+		}
+
+		/// <summary>
+		/// Handles the Click event of the btnBrowseForReplaceRootDirectory control.
+		/// </summary>
+		/// <param name="sender">The source of the event.</param>
+		/// <param name="e">The <see cref="System.Windows.RoutedEventArgs"/> instance containing the event data.</param>
+		private void btnBrowseForReplaceRootDirectory_Click(object sender, RoutedEventArgs e)
+		{
+			// Setup the prompt
+			var folderDialog = new System.Windows.Forms.FolderBrowserDialog();
+			folderDialog.Description = "Select the directory that you want to use to replace the root directory in the returned paths...";
+			folderDialog.ShowNewFolderButton = false;
+
+			// If the user selected a folder, put it in the Replace Root Directory text box.
+			if (folderDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+				txtReplaceRootDirectory.Text = folderDialog.SelectedPath;
+		}
+
+		/// <summary>
+		/// Handles the Click event of the btnGetPathLengths control.
+		/// </summary>
+		/// <param name="sender">The source of the event.</param>
+		/// <param name="e">The <see cref="System.Windows.RoutedEventArgs"/> instance containing the event data.</param>
+		private void btnGetPathLengths_Click(object sender, RoutedEventArgs e)
+		{
+			GetPaths();
+		}
+
+		/// <summary>
+		/// Gets the paths and displays them on the UI.
+		/// </summary>
+		private void GetPaths()
+		{
+			var rootDirectory = txtRootDirectory.Text.Trim();
+			var rootDirectoryReplacement = txtReplaceRootDirectory.Text.Trim();
+			var searchPattern = txtSearchPattern.Text;
+			int minPathLength = numMinPathLength.Value ?? 0;
+			int maxPathLength = numMaxPathLength.Value ?? 999999;
+
+			if (!Directory.Exists(rootDirectory))
+			{
+				MessageBox.Show(string.Format("The Root Directory \"{0}\" does not exist. Please specify a valid directory.", rootDirectory), "Invalid Root Directory");
+				return;
+			}
+
+			// Clear any previous paths out.
+			this.Paths = new List<PathInfo>();
+			txtNumberOfPaths.Text = string.Empty;
+			txtMinAndMaxPathLengths.Text = string.Empty;
+
+			// If we should NOT be replacing the Root Directory text, make sure we don't pass anything in for that parameter.
+			if (!(chkReplaceRootDirectory.IsChecked ?? false))
+				rootDirectoryReplacement = null;
+
+			// Build the options to search with.
+			var searchOptions = new PathLengthSearchOptions()
+			{
+				RootDirectory = rootDirectory,
+				SearchPattern = searchPattern,
+				SearchOption = (chkIncludeSubdirectories.IsChecked ?? false) ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly,
+				TypesToGet = (FileSystemTypes)cmbTypesToInclude.SelectedValue,
+				RootDirectoryReplacement = rootDirectoryReplacement,
+				MinimumPathLength = minPathLength,
+				MaximumPathLength = maxPathLength
+			};
+
+			try
+			{
+				// Get and display the new paths.
+				Paths = PathLengthChecker.PathLengthChecker.GetPathsWithLengths(searchOptions).ToList();
+
+				// Display the number of paths found.
+				txtNumberOfPaths.Text = Paths.Count + " Paths Found";
+
+				// Display the shortest and longest path lengths.
+				int shortestPathLength = Paths.Count > 0 ? Paths.Min(p => p.Length) : 0;
+				int longestPathLength = Paths.Count > 0 ? Paths.Max(p => p.Length) : 0;
+				txtMinAndMaxPathLengths.Text = string.Format("Shortest Path: {0}, Longest Path: {1} characters", shortestPathLength, longestPathLength);
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message, "Error Occurred");
+			}
+		}
+
+		/// <summary>
+		/// Handles the Click event of the btnCopyToClipboardWithoutLengths control.
+		/// </summary>
+		/// <param name="sender">The source of the event.</param>
+		/// <param name="e">The <see cref="System.Windows.RoutedEventArgs"/> instance containing the event data.</param>
+		private void btnCopyToClipboardWithoutLengths_Click(object sender, RoutedEventArgs e)
+		{
+			var text = new StringBuilder();
+			foreach (var path in Paths)
+			{
+				text.AppendLine(path.Path);
+			}
+			Clipboard.SetText(text.ToString());
+		}
+
+		/// <summary>
+		/// Handles the Click event of the btnCopyToClipboard control.
+		/// </summary>
+		/// <param name="sender">The source of the event.</param>
+		/// <param name="e">The <see cref="System.Windows.RoutedEventArgs"/> instance containing the event data.</param>
+		private void btnCopyToClipboard_Click(object sender, RoutedEventArgs e)
+		{
+			var text = new StringBuilder();
+			foreach (var path in Paths)
+			{
+				text.AppendLine(path.ToString());
+			}
+			Clipboard.SetText(text.ToString());
+		}
+	}
+}
