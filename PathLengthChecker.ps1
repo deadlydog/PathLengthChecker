6e7b18710ac3ea83e2675ff9340d0b15af2927be
deadlydog <deadlydog@hotmail.com>
2020-08-07 09:49:47 -0600

    Update PowerShell script a bit more

--- PathLengthChecker.ps1
+++ PathLengthChecker.ps1
@@ -1,17 +1,11 @@
-<# PathLengthChecker.ps1
-	.SYNOPSIS
-		Output the length of all files and folders in the given path.
-
-	.EXAMPLE
-		PathLengthChecker -PathToScan "C:\example" -OutputFilePath "C:\temp\PathLengths.txt" -WriteToConsole $false -ShowShortPaths $false
-#>
+# Output the length of all files and folders in the given directory path.
 [CmdletBinding()]
 param
 (
 	[Parameter(HelpMessage = 'The directory to scan path lengths in. Subdirectories will be scanned as well.')]
-	[string] $DirectoryPathToScan = "C:\Temp",
+	[string] $DirectoryPathToScan = 'C:\Temp',
 
-	[Parameter(HelpMessage = 'Only paths this length or longer will be included in the results.')]
+	[Parameter(HelpMessage = 'Only paths this length or longer will be included in the results. Set this to 260 to find problematic paths in Windows.')]
 	[int] $MinimumPathLengthsToShow = 0,
 
 	[Parameter(HelpMessage = 'If the results should be written to the console or not. Can be slow if there are many results.')]
@@ -21,35 +15,41 @@ param
 	[bool] $WriteResultsToGridView = $true,
 
 	[Parameter(HelpMessage = 'If the results should be written to a file or not.')]
-	[bool] $WriteResultsToFile = $true,
+	[bool] $WriteResultsToFile = $false,
 
 	[Parameter(HelpMessage = 'The file path to write the results to when $WriteResultsToFile is true.')]
-	[string] $ResultsFilePath = "C:\Temp\PathLengths.txt"
+	[string] $ResultsFilePath = 'C:\Temp\PathLengths.txt'
 )
 
 # Ensure output directory exists
 [string] $resultsFileDirectoryPath = Split-Path $ResultsFilePath -Parent
 if (!(Test-Path $resultsFileDirectoryPath)) { New-Item $resultsFileDirectoryPath -ItemType Directory }
 
-# Open a new file stream (nice and fast) and write all the paths and their lengths to it.
+# Open a new file stream (nice and fast) to write all the paths and their lengths to it.
 if ($WriteResultsToFile) { $fileStream = New-Object System.IO.StreamWriter($ResultsFilePath, $false) }
 
 $filePathsAndLengths = [System.Collections.ArrayList]::new()
 
-Get-ChildItem -Path $DirectoryPathToScan -Recurse -Force | Select-Object -Property FullName, @{Name = "FullNameLength"; Expression = { ($_.FullName.Length) } } | Sort-Object -Property FullNameLength -Descending | ForEach-Object {
+# Get all file and directory paths and write them if applicable.
+Get-ChildItem -Path $DirectoryPathToScan -Recurse -Force |
+	Select-Object -Property FullName, @{Name = "FullNameLength"; Expression = { ($_.FullName.Length) } } |
+	Sort-Object -Property FullNameLength -Descending |
+	ForEach-Object {
+
 	$filePath = $_.FullName
 	$length = $_.FullNameLength
 
-	# If this path is too short, skip it and move onto the next one.
-	if ($length -lt $MinimumPathLengthsToShow) { continue }
+	# If this path is long enough, add it to the results.
+	if ($length -ge $MinimumPathLengthsToShow)
+	{
+		[string] $lineOutput = "$length : $filePath"
 
-	[string] $lineOutput = "$length : $filePath"
+		if ($WriteResultsToConsole) { Write-Output $lineOutput }
 
-	if ($WriteResultsToConsole) { Write-Output $lineOutput }
+		if ($WriteResultsToFile) { $fileStream.WriteLine($lineOutput) }
 
-	if ($WriteResultsToFile) { $fileStream.WriteLine($lineOutput) }
-
-	$filePathsAndLengths.Add($_) > $null
+		$filePathsAndLengths.Add($_) > $null
+	}
 }
 
 if ($WriteResultsToFile) { $fileStream.Close() }
