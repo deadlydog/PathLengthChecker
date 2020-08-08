3502394d68f0d3c2c4a738c56b56588c7e6d79c6
deadlydog <deadlydog@hotmail.com>
2020-08-08 00:58:43 -0600

    feat: Allow users to choose search strategy. fix: Show IntegerUpDown tooltips for 60 seconds. feat: Improve error handling and messages a bit.

--- src/PathLengthChecker/PathRetriever.cs
+++ src/PathLengthChecker/PathRetriever.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using System.Threading;
 using SearchOption = System.IO.SearchOption;
 
@@ -16,6 +17,21 @@ namespace PathLengthChecker
 		Files = 1,
 		Directories = 2,
 		All = Files | Directories
+	}
+
+	public enum FileSystemSearchStrategies
+	{
+		/// <summary>
+		/// Default .Net implementation.
+		/// Very fast, but haults when it encounters a directory it doesn't have permissions to go into.
+		/// </summary>
+		SystemIo = 1,
+
+		/// <summary>
+		/// 3rd party implementation.
+		/// Safer in that it is able to enumerate over all directories, but slower.
+		/// </summary>
+		AlphaFs = 2
 	}
 
 	/// <summary>
@@ -48,6 +64,11 @@ namespace PathLengthChecker
 		/// Specify null to leave the original paths unmodified.
 		/// </summary>
 		public string RootDirectoryReplacement = null;
+
+		/// <summary>
+		/// Which implementation to use for searching the file system.
+		/// </summary>
+		public FileSystemSearchStrategies FileSystemSearchStrategy = FileSystemSearchStrategies.SystemIo;
 	}
 
 	/// <summary>
@@ -71,21 +92,17 @@ namespace PathLengthChecker
 				searchOptions.SearchPattern = "*";
 
 			// Get the paths according to the search parameters
-			IEnumerable<string> paths;
-			try
+			IEnumerable<string> paths = Enumerable.Empty<string>();
+			switch (searchOptions.FileSystemSearchStrategy)
 			{
-				DirectoryEnumerationOptions options = (DirectoryEnumerationOptions)searchOptions.TypesToGet |
-					DirectoryEnumerationOptions.ContinueOnException | DirectoryEnumerationOptions.SkipReparsePoints;
+				case FileSystemSearchStrategies.AlphaFs:
+					paths = GetPathsUsingAlphaFs(searchOptions);
+					break;
 
-				if (searchOptions.SearchOption == SearchOption.AllDirectories)
-					options |= DirectoryEnumerationOptions.Recursive;
-
-				paths = Directory.EnumerateFileSystemEntries(searchOptions.RootDirectory, searchOptions.SearchPattern, options);
-			}
-			catch (Exception ex)
-			{
-				Debug.Print(ex.Message);
-				yield break;
+				default:
+				case FileSystemSearchStrategies.SystemIo:
+					paths = GetPathsUsingSystemIo(searchOptions);
+					break;
 			}
 
 			// Return each of the paths, replacing the Root Directory if specified to do so.
@@ -100,6 +117,42 @@ namespace PathLengthChecker
 				else
 					yield return path.Replace(searchOptions.RootDirectory, searchOptions.RootDirectoryReplacement);
 			}
+		}
+
+		private static IEnumerable<string> GetPathsUsingAlphaFs(PathSearchOptions searchOptions)
+		{
+			DirectoryEnumerationOptions options = (DirectoryEnumerationOptions)searchOptions.TypesToGet |
+				DirectoryEnumerationOptions.ContinueOnException | DirectoryEnumerationOptions.SkipReparsePoints;
+
+			if (searchOptions.SearchOption == SearchOption.AllDirectories)
+				options |= DirectoryEnumerationOptions.Recursive;
+
+			var paths = Directory.EnumerateFileSystemEntries(searchOptions.RootDirectory, searchOptions.SearchPattern, options);
+			return paths;
+		}
+
+		private static IEnumerable<string> GetPathsUsingSystemIo(PathSearchOptions searchOptions)
+		{
+			// Get the paths according to the search parameters
+			var paths = Enumerable.Empty<string>();
+
+			switch (searchOptions.TypesToGet)
+			{
+				default:
+				case FileSystemTypes.All:
+					paths = Directory.GetFileSystemEntries(searchOptions.RootDirectory, searchOptions.SearchPattern, searchOptions.SearchOption);
+					break;
+
+				case FileSystemTypes.Directories:
+					paths = Directory.GetDirectories(searchOptions.RootDirectory, searchOptions.SearchPattern, searchOptions.SearchOption);
+					break;
+
+				case FileSystemTypes.Files:
+					paths = Directory.GetFiles(searchOptions.RootDirectory, searchOptions.SearchPattern, searchOptions.SearchOption);
+					break;
+			}
+
+			return paths;
 		}
 	}
 }
