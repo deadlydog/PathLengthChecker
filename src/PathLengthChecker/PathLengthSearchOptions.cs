08ff009c938101ccc3b28d967186bb06464fd210
deadlydog <deadlydog@hotmail.com>
2020-10-20 00:13:38 -0600

    fix: Fix GUI descrepencies between showing -1 and 999999 for MaxPathLength

--- src/PathLengthChecker/PathLengthSearchOptions.cs
+++ src/PathLengthChecker/PathLengthSearchOptions.cs
@@ -7,15 +7,17 @@
 	{
 		/// <summary>
 		/// The Minimum Length that a Path must have to be included in the search results.
-		/// Specify a value of -1 to ignore the minimum path length.
+		/// Specify a value of 0 to ignore the minimum path length.
 		/// </summary>
-		public int MinimumPathLength = -1;
+		public int MinimumPathLength = 0;
 
 		/// <summary>
 		/// The Maximum Length that a Path must have to be included in the search results.
 		/// Specify a value of -1 to ignore the maximum path length.
 		/// </summary>
-		public int MaximumPathLength = -1;
+		public int MaximumPathLength = MaximumPathLengthMaxValue;
+
+		public const int MaximumPathLengthMaxValue = 999999;
 
 		/// <summary>
 		/// Indicates the type of result that is output once the search completes.
